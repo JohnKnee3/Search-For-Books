350578da41b9e95468a49501d220f68b0199bb6c
john.a.clark3 <john.a.clark3@gmail.com>
2022-05-06 12:02:41 -0600

    type defs may be set up
    need to actually add a user and then trouble shoot if needed.  Can't do much with no info

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -13,10 +13,35 @@ const typeDefs = gql`
   type Book {
     bookId: String
     authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type bookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
   }
 
   type Query {
     users: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(book: bookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 // export the typeDefs
